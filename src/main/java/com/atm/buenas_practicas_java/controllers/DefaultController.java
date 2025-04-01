30de28592bd386a7a754eaf9a1cf9f91238233e1
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-04-01 20:45:55 +0200

    Changed to layouts and fragments

--- src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
@@ -110,4 +110,19 @@ public class DefaultController {
         return "redirect:/entities";
     }
 
+    @GetMapping({"", "/"})
+    public String getHomePage(){
+        return "index";
+    }
+
+    @GetMapping({"", "/concerts"})
+    public String getConcertPage(){
+        return "/concert/index";
+    }
+
+    @GetMapping({"", "/artists"})
+    public String getArtistPage(){
+        return "/artist/artist";
+    }
+
 }
