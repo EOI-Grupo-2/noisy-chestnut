d50db7222b4a32a132d745536d4286f809efc4af
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-04-08 19:00:26 +0200

    First approach to chat view

--- src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
@@ -124,4 +124,9 @@ public class DefaultController {
     public String getArtistPage(){
         return "/artist/artist";
     }
+
+    @GetMapping({"", "/chat"})
+    public String getChatPage(){
+        return "/social/chat";
+    }
 }
