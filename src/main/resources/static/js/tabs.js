92a50950bb879d24aeb30da5223240eaba23ae72
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-04-22 19:23:24 +0200

    Added tab functionality

--- src/main/resources/static/js/tabs.js
+++ src/main/resources/static/js/tabs.js
@@ -1,8 +1,7 @@
-function selectTab(tab){
-    const elements = document.getElementsByClassName()
+function selectTab(tab, className){
+    const elements = document.getElementsByClassName(className)
     for (element of elements){
-        element.style.visibility = element.id === tag ? 'visible' : 'hidden';
-        element.style.opacity = element.id === tag ? '1' : '0';
+        element.style.visibility = element.id === tab ? 'visible' : 'hidden';
+        element.style.opacity = element.id === tab ? '1' : '0';
     }
-
 }
