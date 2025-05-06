193fffab5f19d836a3a109109972ca0b896b206e
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-05-06 19:40:58 +0200

    Modal functionality improved

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,6 +1,6 @@
 function openModal(elementId) {
     const chatModal = document.getElementById(elementId);
-    let isClose = chatModal.style.visibility === 'hidden' || chatModal.style.opacity === '0';
+    let isClose = chatModal.style.visibility === 'hidden' || chatModal.style.opacity === '0' || chatModal.style.opacity === '' || chatModal.style.visibility === '';
     chatModal.style.visibility = isClose ? 'visible' : 'hidden';
     chatModal.style.opacity = isClose ? '1' : '0';
 }
