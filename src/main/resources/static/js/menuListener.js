faf6263b1ec0b1d124e7087229faa582a9009228
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-05-13 18:49:30 +0200

    Added open and close animation to menu

--- src/main/resources/static/js/menuListener.js
+++ src/main/resources/static/js/menuListener.js
@@ -1,8 +1,8 @@
 window.addEventListener('resize', () => {
     const menu = document.getElementById('menuModal');
-    if(window.innerWidth > 992){
+    if(window.innerWidth > 992 && menu.style.transform === 'translateX(-15rem)'){
         menu.style.transform = 'translateX(0rem)'
-    } else if(window.innerWidth <= 992){
+    } else if(window.innerWidth <= 992 && menu.style.transform === 'translateX(0rem)'){
         menu.style.transform = 'translateX(-15rem)'
     }
 })
