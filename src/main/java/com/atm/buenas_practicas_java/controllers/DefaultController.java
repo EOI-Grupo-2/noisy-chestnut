348d3a0bfc919643fd41c249327e26cdbef4007b
AntonioSala2 <antoniomanuel271101@gmail.com>
2025-06-02 00:47:32 +0200

    Añadido el panel de administracion de conciertos, parecido al de usuarios

--- src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/DefaultController.java
@@ -207,10 +207,9 @@ public class DefaultController {
     }
 
     @GetMapping("/places")
-    public String showPlacesPage() {
-        return "/places/places";
-    }
+    public String showPlacesPage() {return "/places/places"; }
+
+    @GetMapping("/concert/admin")
+    public String showConcertsAdminPage() {return "concert/concertsAdmin"; }
 }
-
-
 
