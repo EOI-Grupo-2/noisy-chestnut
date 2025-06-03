bb74f12466626b2ca4fb5142cfc8352efb141e0b
inmagdg <inmagdg23@gmail.com>
2025-06-03 17:32:52 +0200

    Entidades creadas.

---
+++ src/main/java/com/atm/buenas_practicas_java/entities/Publications.java
@@ -0,0 +1,38 @@
+package com.atm.buenas_practicas_java.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String title;
+
+    @Column (nullable = false)
+    private String description;
+
+    @Column (nullable = false)
+    private String photoUrl;
+
+    @Column(nullable = false)
+    private Integer likes;
+
+    @JoinColumn (name = "pubDate", nullable = false)
+    private LocalDateTime Date;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn (name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/atm/buenas_practicas_java/entities/Role.java
@@ -0,0 +1,23 @@
+package com.atm.buenas_practicas_java.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column (nullable = false)
+    private Integer privilegesLevel;
+}
