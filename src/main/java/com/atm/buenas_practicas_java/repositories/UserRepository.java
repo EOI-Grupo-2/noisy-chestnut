993e675e878e9b5df5e38d32e2c2610a6a09b3dd
IvanRuizNavarro47 <145495051+IvanRuizNavarro47@users.noreply.github.com>
2025-06-04 17:51:12 +0200

    Servicio de roles RoleService.java

--- src/main/java/com/atm/buenas_practicas_java/repositories/UserRepository.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.atm.buenas_practicas_java.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByRole_Name(String name);
+
 }
--- src/main/java/com/atm/buenas_practicas_java/services/RoleService.java
+++ src/main/java/com/atm/buenas_practicas_java/services/RoleService.java
@@ -1,13 +1,18 @@
 package com.atm.buenas_practicas_java.services;
 
 import com.atm.buenas_practicas_java.entities.Role;
+import com.atm.buenas_practicas_java.entities.User;
 import com.atm.buenas_practicas_java.repositories.RoleRepository;
+import com.atm.buenas_practicas_java.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
 
     private RoleRepository roleRepository;
+    private  UserRepository userRepository;
 
     public RoleService(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
@@ -20,4 +25,36 @@ public class RoleService {
     public Role findByName(String name) {
         return roleRepository.findByName(name);
     }
+
+
+    // Listar todos los roles
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    // Obtener usuarios por nombre de rol
+    public List<User> getUsersByRoleName(String roleName) {
+        return userRepository.findByRole_Name(roleName);
+    }
+
+
+    // Asignar rol a un usuario
+    public User assignRoleToUser(Long userId, Long roleId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        Role role = roleRepository.findById(roleId)
+                .orElseThrow(() -> new RuntimeException("Rol no encontrado"));
+        user.setRole(role);
+        return userRepository.save(user);
+    }
+
+    // 5. Eliminar un rol
+    public void deleteRole(Long id) {
+        if (!roleRepository.existsById(id)) {
+            throw new RuntimeException("Rol no encontrado");
+        }
+        roleRepository.deleteById(id);
+    }
+
 }
+
