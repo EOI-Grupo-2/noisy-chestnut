c6cec7a84ff7028110115b91cf8c281bb4f371c6
inmagdg <inmagdg23@gmail.com>
2025-06-05 18:45:24 +0200

    Servicio de Publications terminado.

---
+++ src/main/java/com/atm/buenas_practicas_java/services/PublicationsService.java
@@ -0,0 +1,18 @@
+package com.atm.buenas_practicas_java.services;
+
+import com.atm.buenas_practicas_java.DTO.PublicationsDTO;
+import com.atm.buenas_practicas_java.entities.Publications;
+import com.atm.buenas_practicas_java.entities.User;
+import com.atm.buenas_practicas_java.repositories.PublicationsRepository;
+import com.atm.buenas_practicas_java.services.mapper.PublicationsMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PublicationsService extends AbstractBusinessService<Publications, Long, PublicationsDTO, PublicationsRepository, PublicationsMapper> {
+
+    public PublicationsService(PublicationsRepository repository, PublicationsMapper mapper) {
+        super(repository, mapper);
+    }
+}
