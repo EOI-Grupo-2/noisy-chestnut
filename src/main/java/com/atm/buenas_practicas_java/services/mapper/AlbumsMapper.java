091840880312ba671255bccdaf57e8b1a163fd87
inmagdg <inmagdg23@gmail.com>
2025-06-05 19:19:31 +0200

    Servicio de Albums terminado.

--- src/main/java/com/atm/buenas_practicas_java/services/mapper/AlbumsMapper.java
+++ src/main/java/com/atm/buenas_practicas_java/services/mapper/AlbumsMapper.java
@@ -3,37 +3,26 @@ package com.atm.buenas_practicas_java.services.mapper;
 import com.atm.buenas_practicas_java.DTO.AlbumsDTO;
 import com.atm.buenas_practicas_java.entities.Albums;
 import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
 
-@Component
+@Service
 public class AlbumsMapper extends AbstractServiceMapper<Albums, AlbumsDTO> {
 
     @Override
     public Albums toEntity(AlbumsDTO dto) {
         Albums album = new Albums();
         ModelMapper mapper = new ModelMapper();
-        mapper.map(album, dto);
-        album.setId(dto.getId());
-        album.setTitle(dto.getTitle());
-        album.setRating(new DecimalFormat(dto.getRating()));
-        album.setSpotifyLink(dto.getSpotifyLink());
-        album.setTotalTracks(dto.getTotalTracks());
-        album.setDate(dto.getDate());
+        mapper.map(dto, album);
         return album;
     }
 
     @Override
-    public AlbumsDTO toDto(Albums entity) {
+    public AlbumsDTO toDto(Albums albums) {
         AlbumsDTO dto = new AlbumsDTO();
-        dto.setId(entity.getId());
-        dto.setTitle(entity.getTitle());
-        dto.setRating(entity.getRating().toPattern());
-        dto.setSpotifyLink(entity.getSpotifyLink());
-        dto.setTotalTracks(entity.getTotalTracks());
-        dto.setDate(entity.getDate());
-        dto.setUserId(entity.getUser().getId());
+        ModelMapper mapper = new ModelMapper();
+        mapper.map(albums, dto);
         return dto;
     }
 }
