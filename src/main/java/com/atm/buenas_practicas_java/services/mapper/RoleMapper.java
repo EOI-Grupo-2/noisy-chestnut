f6f766fefefaa3bf14b5571eacdad820428e864b
IvanRuizNavarro47 <145495051+IvanRuizNavarro47@users.noreply.github.com>
2025-06-06 14:32:27 +0200

    Cambios finales Service Roles

--- src/main/java/com/atm/buenas_practicas_java/services/mapper/RoleMapper.java
+++ src/main/java/com/atm/buenas_practicas_java/services/mapper/RoleMapper.java
@@ -2,36 +2,29 @@ package com.atm.buenas_practicas_java.services.mapper;
 
 import com.atm.buenas_practicas_java.DTO.RoleDTO;
 import com.atm.buenas_practicas_java.entities.Role;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class RoleMapper extends AbstractServiceMapper<Role, RoleDTO> {
 
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public RoleDTO toDto(Role entity) {
         if (entity == null) return null;
-        return new RoleDTO(entity.getId(), entity.getName());
+        return modelMapper.map(entity, RoleDTO.class);
     }
 
     @Override
     public Role toEntity(RoleDTO dto) {
         if (dto == null) return null;
-        Role role = new Role();
-        role.setId(dto.getId());
-        role.setName(dto.getName());
-        return role;
-    }
-
-    @Override
-    public List<RoleDTO> toDto(List<Role> entityList) {
-        return entityList.stream().map(this::toDto).collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Role> toEntity(List<RoleDTO> dtoList) {
-        return dtoList.stream().map(this::toEntity).collect(Collectors.toList());
+        return modelMapper.map(dto, Role.class);
     }
 }
