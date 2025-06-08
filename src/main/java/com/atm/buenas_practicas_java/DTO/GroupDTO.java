61cbdb7f07041ca3237b045f971ad9e897ff4550
inmagdg <inmagdg23@gmail.com>
2025-06-08 20:52:21 +0200

    Servicio de Group completado.

---
+++ src/main/java/com/atm/buenas_practicas_java/DTO/GroupDTO.java
@@ -0,0 +1,16 @@
+package com.atm.buenas_practicas_java.DTO;
+
+import com.atm.buenas_practicas_java.entities.Concert;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private Concert concertId;
+}
