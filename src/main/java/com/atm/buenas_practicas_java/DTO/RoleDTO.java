afefc98033f9b60fa1f14453d771bd8239fc6b25
Andreu Montagut Osuna <80252401+andmonosu@users.noreply.github.com>
2025-06-09 12:50:47 +0200

    Update RoleDTO.java

--- src/main/java/com/atm/buenas_practicas_java/DTO/RoleDTO.java
+++ src/main/java/com/atm/buenas_practicas_java/DTO/RoleDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.atm.buenas_practicas_java.entities.User;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
