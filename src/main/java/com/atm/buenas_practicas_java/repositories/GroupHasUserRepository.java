152e1bee535b8e81ea5bcf5378f7627a86aab5bc
IvanRuizNavarro47 <145495051+IvanRuizNavarro47@users.noreply.github.com>
2025-06-09 11:19:22 +0200

    Eliminación de GroupHasUserId.java y cambio Para Long Id

--- src/main/java/com/atm/buenas_practicas_java/repositories/GroupHasUserRepository.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/GroupHasUserRepository.java
@@ -1,10 +1,9 @@
 package com.atm.buenas_practicas_java.repositories;
 
 import com.atm.buenas_practicas_java.entities.GroupsHasUser;
-import com.atm.buenas_practicas_java.entities.GroupHasUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupHasUserRepository extends JpaRepository<GroupsHasUser, GroupHasUserId> {
+public interface GroupHasUserRepository extends JpaRepository<GroupsHasUser, Long> {
 }
--- src/main/java/com/atm/buenas_practicas_java/services/GroupsHasUserService.java
+++ src/main/java/com/atm/buenas_practicas_java/services/GroupsHasUserService.java
@@ -1,7 +1,6 @@
 package com.atm.buenas_practicas_java.services;
 
 import com.atm.buenas_practicas_java.DTO.GroupsHasUserDTO;
-import com.atm.buenas_practicas_java.entities.GroupHasUserId;
 import com.atm.buenas_practicas_java.entities.GroupsHasUser;
 import com.atm.buenas_practicas_java.repositories.GroupHasUserRepository;
 import com.atm.buenas_practicas_java.services.mapper.GroupsHasUserMapper;
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupsHasUserService extends AbstractBusinessService<
         GroupsHasUser,
-        GroupHasUserId,
+        Long,
         GroupsHasUserDTO,
         GroupHasUserRepository,
         GroupsHasUserMapper> {
